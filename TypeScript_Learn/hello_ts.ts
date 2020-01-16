b8387a45b32e8f5c333611fbd79cca86442bd529
明乃超 <mingnaichao@qq.com>
2020-01-16 20:41:56 +0800

    ts学习

---
+++ TypeScript_Learn/hello_ts.ts
@@ -0,0 +1,52 @@
+/* 
+const hello: string = 'hello world!'
+console.log(hello)
+
+class Site {
+    name(): void {
+        console.log('hello')
+    }
+}
+var obj = new Site();
+obj.name()
+
+let arr: number[] = [1, 2];
+let x: [string, number];
+x = ['hello', 1];
+
+var uname: string = 'Runoob';
+var uname: string;
+var uname = 'Runoob';
+
+var global_num = 2; //全局变量
+class Numbers {
+    num_val = 13; //实例变量
+    static sval = 10; //静态变量
+
+    storeNum(): void {
+        var local_num = 14; //局部变量
+    }
+}
+
+function greet(): string {
+    return 'hello world'
+}
+
+function add(x: number, y: number): number {
+    return x + y
+}
+add(1, 2);
+
+class Car {
+    engine: string;
+    constructor(engine: string) {
+        this.engine = engine;
+    };
+    disp(): void {
+        console.log("发动机为：" + this.engine)
+    }
+}
+
+*/
+
+let isDone: boolean = false;
